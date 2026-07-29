8b754e99668c57874fdc8de2cfc6bfc2383b7513
Marlee Lyle <marleeannlyle1@gmail.com>
2026-07-29 12:31:36 -0400

    Add lot_view input to current_map reactive function

--- app.R
+++ app.R
@@ -3,12 +3,14 @@ source("R/map_builder.R")
 current_map <- reactive({
 
   build_map(
+    spatial_points = spatial_points(),
     ecoregions_all = ecoregions_all,
     world = world_data(),
-    spatial_points = spatial_points(),
     eco_mode = input$eco_mode,
     selected_eco = selected_eco(),
-    point_size = input$point_size
+    point_size = input$point_size,
+    lot_view = input$lot_view,
+    eco_pal = eco_pal()
   )
 
 })
